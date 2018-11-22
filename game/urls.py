ace7aad98a94820775de4aa96d1fdc960aee5715
guih2127 <guih2127@gmail.com>
2018-11-22 01:28:57 -0200

    profile template and other changes

--- game/urls.py
+++ game/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('wishlist/<int:pk>', views.add_game_to_wishlist, name='wishlist'),
     path('finished/<int:pk>', views.add_game_to_finished, name='finished'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<int:pk>', views.profile, name='profile'),
     path('new_review/<int:pk>', views.new_review, name='new_review')
 ]
--- game/views.py
+++ game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Game, Developer, Genre, Platform, Mode, UserGamesInformation, Review
+from .models import Game, Developer, Genre, Platform, Mode, UserGamesInformation, Review, User
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
@@ -37,16 +37,18 @@ def game_list(request):
     return render(request, 'game/game_list.html', {'games': games})
 
 @login_required
-def profile(request):
-    user = request.user
+def profile(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user_reviews = Review.objects.filter(author=user)
     user_games = UserGamesInformation.objects.get_or_create(pk=user.id)
     user_games = UserGamesInformation.objects.get(pk=user.id)
     wishlist = user_games.want_to_play.order_by()[0:5]
     currently_playing = user_games.currently_playing.order_by()[0:5]
     finished = user_games.finished.order_by()[0:5]
 
-    return render(request, 'game/profile.html', {'wishlist': wishlist, 
-    'currently_playing': currently_playing, 'finished': finished, 'user': user})
+    return render(request, 'game/profile.html', {'user_reviews': user_reviews,
+    'user_games': user_games, 'wishlist': wishlist, 'currently_playing': currently_playing,
+    'finished': finished})
 
 def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)

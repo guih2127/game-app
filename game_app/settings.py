e4855495bc31ac82f51099a888b431d1e82921d9
guih2127 <guih2127@gmail.com>
2018-11-26 15:58:15 -0200

    favicon

--- game_app/settings.py
+++ game_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '3-_v^$j51808lt=1a&5&w2$&767v3@4sz%=+m(p7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)

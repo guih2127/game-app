5d8f1beda5473bda4b3e7f447dad603a6f266f58
guih2127 <guih2127@gmail.com>
2019-03-25 01:30:18 -0300

    start of the seed file, to provide initial data for the models

---
+++ seed.py
@@ -0,0 +1,51 @@
+from game.models import *
+
+def erase_data():
+	Game.objects.all().delete()
+	Developer.objects.all().delete()
+	Genre.objects.all().delete()
+	Platform.objects.all().delete()
+	Mode.objects.all().delete()
+	Friends.objects.all().delete()
+	UserRequests.objects.all().delete()
+	UserGamesInformation.objects.all().delete()
+
+def create_platforms():
+	Platform.objects.bulk_create(
+		[Platform(name="Playstation 4"),
+		Platform(name="Xbox One"),
+		Platform(name="Playstation 3"),
+		Platform(name="Xbox 360"),
+		Platform(name="PC")]
+	)
+
+def create_genres():
+	Genre.objects.bulk_create(
+		[Genre(name="RPG"),
+		Genre(name="Action"),
+		Genre(name="Puzzle"),
+		Genre(name="Fantasy"),
+		Genre(name="Japanese"),
+		Genre(name="Fighting"),
+		Genre(name="Indie"),
+		Genre(name="Racing"),
+		Genre(name="Soccer"),
+		Genre(name="Sports"),
+		Genre(name="MMORPG"),
+		Genre(name="Rhythm"),
+		Genre(name="Survival"),
+		Genre(name="Stealth"),
+		Genre(name="Shooter"),
+		Genre(name="FPS"),
+		Genre(name="Platform"),
+		Genre(name="Simulation"),
+		]
+	)
+
+def create_modes():
+	Mode.objects.bulk_create(
+		[Mode(name="Online"),
+		Mode(name="Offline")
+		]
+	)
+
